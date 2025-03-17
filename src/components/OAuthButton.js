f170b6482927e1ad99ba00da8db5cec6c461f8c4
josephecombs <joseph.e.combs@gmail.com>
2025-03-17 18:19:18 -0400

    try to actually get localhost:8787 working next

--- src/components/OAuthButton.js
+++ src/components/OAuthButton.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 function OAuthButton() {
+  const isLocal = process.env.NODE_ENV === 'development';
+  const oauthUrl = isLocal ? 'http://localhost:8787/sessions/new' : '/sessions/new';
+
   return (
     <div>
-      <a href="/sessions/new">Login with Google</a>
+      <a href={oauthUrl}>Login with Google</a>
     </div>
   );
 }
