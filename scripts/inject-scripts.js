b56736dd06fd499df8e05b8b16d39ce5743a4ab1
josephecombs <joseph.e.combs@gmail.com>
2025-03-30 12:01:22 -0400

    make static pages work with github pages

--- scripts/inject-scripts.js
+++ scripts/inject-scripts.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
+// List of static pages that need script injection
+const staticPages = [
+  'privacy-policy.html',
+  'terms-of-service.html',
+  '404.html'
+];
+
 // Read the asset manifest
 const manifestPath = path.join(__dirname, '../build/asset-manifest.json');
 const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
-
-// Read the privacy policy HTML
-const privacyPolicyPath = path.join(__dirname, '../build/privacy-policy.html');
-let privacyPolicy = fs.readFileSync(privacyPolicyPath, 'utf8');
 
 // Get the main script and CSS files
 const mainJs = manifest.files['main.js'];
@@ -17,10 +20,15 @@ const mainCss = manifest.files['main.css'];
 const scriptTag = `<script defer src="${mainJs}"></script>`;
 const cssTag = `<link rel="stylesheet" href="${mainCss}">`;
 
-// Insert the tags before the closing head tag
-privacyPolicy = privacyPolicy.replace('</head>', `${cssTag}\n    ${scriptTag}\n  </head>`);
-
-// Write back the modified HTML
-fs.writeFileSync(privacyPolicyPath, privacyPolicy);
-
-console.log('Successfully injected scripts into privacy-policy.html'); 
+// Process each static page
+staticPages.forEach(pageName => {
+  const pagePath = path.join(__dirname, '../build', pageName);
+  let pageContent = fs.readFileSync(pagePath, 'utf8');
+  
+  // Insert the tags before the closing head tag
+  pageContent = pageContent.replace('</head>', `${cssTag}\n    ${scriptTag}\n  </head>`);
+  
+  // Write back the modified HTML
+  fs.writeFileSync(pagePath, pageContent);
+  console.log(`Successfully injected scripts into ${pageName}`);
+}); 
